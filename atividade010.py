450fb589a733a4f487703cb2b68aeb0de7ef5438
pedro01p <tsukki237@gmail.com>
2024-09-11 14:23:25 -0300

    par ou impar

---
+++ atividade010.py
@@ -0,0 +1,5 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar
+n=int(input("coloque um numero"))
+r=n/2
+if r==0:print("esse numero é par")
+else:print("ele é impar")
